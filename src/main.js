17cfe9b2941162cba8bbe171eb823049c373df03
UnstoppableGreen <aleksandr-_kalinin@mail.ru>
2021-12-24 20:37:52 +0300

    delete comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-///import { createApp } from 'vue'
-///import App from './App.vue'
-///import router from './router'
-///import './index.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './index.css'
 
-///createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
 //axios.defaults.headers.get['header-name'] = 'value'
 //app.use(cors({credentials: true}));
